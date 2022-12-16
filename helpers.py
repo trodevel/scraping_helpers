8747d94bfb4af1a551e5e966a1ffe5eed0487fef
trodevel <trodevel@gmail.com>
2022-12-16 14:34:46 +0100

    added missing include

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 #from print_helpers import print_fatal, print_error, print_warning, print_info, print_debug
 
 import time
